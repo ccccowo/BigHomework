c109437618b10cd5f31e9da91ca1e7e658ea599a
holan <holanswide@yeah.net>
2024-03-14 14:53:35 +0800

    throw exception

--- demo/seg/seg_view_set.py
+++ demo/seg/seg_view_set.py
@@ -77,20 +77,24 @@ class SegViewSet(ModelViewSet):
         import http.client
         import json
 
-        conn = http.client.HTTPSConnection(TWIST_SERVER_PATH, TWIST_SERVER_PORT)
-        payload = json.dumps(certs)
-        headers = {
-            'User-Agent': 'Apifox/1.0.0 (https://apifox.com)',
-            'Content-Type': 'application/json',
-            'Accept': '*/*',
-            'Host': TWIST_SERVER_PATH+':'+TWIST_SERVER_PORT,
-            'Connection': 'keep-alive'
-        }
-        conn.request("POST", "/getStuNum", payload, headers)
-        res = conn.getresponse()
-        data = res.read()
-        print(">>> request twist server >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>")
-        print(data.decode("utf-8"))
+        try:
+            conn = http.client.HTTPSConnection(TWIST_SERVER_PATH, TWIST_SERVER_PORT)
+            payload = json.dumps(certs)
+            headers = {
+                'User-Agent': 'Apifox/1.0.0 (https://apifox.com)',
+                'Content-Type': 'application/json',
+                'Accept': '*/*',
+                'Host': TWIST_SERVER_PATH+':'+TWIST_SERVER_PORT,
+                'Connection': 'keep-alive'
+            }
+            conn.request("POST", "/getStuNum", payload, headers)
+            res = conn.getresponse()
+            data = res.read()
+            print(">>> request twist server >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>")
+            print(data.decode("utf-8"))
+        except Exception as e:
+            print(">>> request twist server >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>")
+            print(e)
 
         # 查询
         sql = f"select * from seg_segimg where paper = '{paperId}'"
